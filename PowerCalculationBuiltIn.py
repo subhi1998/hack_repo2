d3eec54b51462fb1a05adfe2c4947ed7673a7d44
subhi.agrawal <subhi.agrawal@salesforce.com>
2025-01-26 20:44:12 +0530

    changes

---
+++ PowerCalculationBuiltIn.py
@@ -0,0 +1,8 @@
+import math
+
+def power_builtin(base, exponent):
+        return math.pow(base, exponent)
+
+# Example Usage
+print(power_builtin(2, 3))  # Output: 8.0
+print(power_builtin(2, -3)) # Output: 0.125
